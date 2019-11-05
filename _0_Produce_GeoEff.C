58960934f7449ee49d801c592531d05ee499c6a3
RenshengWang <rensheng0611@outlook.com>
2019-11-06 00:49:41 +0800

    add a switch tag to decide the function of this script

--- _0_Produce_GeoEff.C
+++ _0_Produce_GeoEff.C
@@ -3,6 +3,7 @@ using namespace TMath;
 
 void _0_Produce_GeoEff()
 {
+  bool Is_ProduceGeoEff = 1;
   string SimData_Path = "/home/zhong/Packages/nptool/Outputs/Simulation/Hira10";
   string SimConfigTag = "ThetaUniform";
   string Hira_BadMap_Version = "V1";
@@ -14,14 +15,14 @@ void _0_Produce_GeoEff()
   
   Hira_GeoEff* Hira_GeoEfficiency = new Hira_GeoEff();
   string RootFileNameForStore = "./GeoEff/f1_GeoEff_"+Hira_BadMap_Version+".root";
-  /*
-  {
+  if(Is_ProduceGeoEff==0)
+  { //this is only for test the Efficiency of the Hira.
     Hira_GeoEfficiency->ReadGeoEffHistogram(RootFileNameForStore);
     Hira_GeoEfficiency->Draw_Info(); //Drw the histograms that this class got.
     double GeoEff = Hira_GeoEfficiency->Get_GeoEff(60); //unit: Deg.
     cout<<"GeoEff : "<<GeoEff<<endl;
   }
-  */
+  else if(Is_ProduceGeoEff==1)
   {//the below is for producing the GeoEff
     Hira_GeoEfficiency->Initial_Hira_BadMapper(Hira_BadMapper);
     string SimFileName = SimData_Path+"/myResult_"+SimConfigTag+".root";
